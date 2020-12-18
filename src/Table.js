4bcb57d5b9c3329bc7d5eff1fe97ec58060381bd
libersolis-dev <manny.hagman@gmail.com>
2020-12-18 02:24:04 -0600

    Remove comments

--- src/Table.js
+++ src/Table.js
@@ -24,11 +24,8 @@ function Table({ columns, data }) {
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
-                // Add the sorting props to control sorting. For this example
-                // we can add them into the header props
                 <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                   {column.render('Header')}
-                  {/* Add a sort direction indicator */}
                   <span>
                     {column.isSorted
                       ? column.isSortedDesc
